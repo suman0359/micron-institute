5660c745c974257356703d79fa95b558573c23d0
Tasfir Hossain Suman <tasfir_suman@yahoo.com>
2015-11-02 18:39:04 +0600

    Update Home page and add owl carousel

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      //autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
+
+
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('#myTabs a[href="#profile"]').tab('show') // Select tab by name
+$('#myTabs a:first').tab('show') // Select first tab
+$('#myTabs a:last').tab('show') // Select last tab
+$('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
