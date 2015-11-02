b16cb8416e143a4ddd5f83c19f1ee05db264782f
Tasfir Hossain Suman <tasfir_suman@yahoo.com>
2015-11-02 19:23:52 +0600

    Modify Index page and css

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
  
-      //autoPlay: 3000, //Set AutoPlay to 3 seconds
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
  
       items : 4,
       itemsDesktop : [1199,3],
