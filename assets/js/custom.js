f5782a3c487fafc4631ba699c41f7eb78d7737be
Tasfir Hossain Suman <tasfir_suman@yahoo.com>
2015-11-15 17:01:22 +0600

    Add course-list page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,6 +9,46 @@ $(document).ready(function() {
       itemsDesktopSmall : [979,3]
  
   });
+
+
+// DropDown hover menu
+
+  $('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
+// -------------------------------
+
+
+
+  //  go to top
+  	var offset = 1000,
+	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+	offset_opacity = 1200,
+	//duration of the top scrolling animation (in ms)
+	scroll_top_duration = 500,
+	//grab the "back to top" link
+	$back_to_top = $('.go-top');
+
+	//hide or show the "back to top" link
+	$(window).on('scroll', function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('go-top-visible') : $back_to_top.removeClass('go-top-visible go-top-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('go-top-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
  
 });
 
@@ -31,9 +71,11 @@ var mapCanvas = document.getElementById('map');
 var mapOptions = {
   center: new google.maps.LatLng(23.744173, 90.413775),
   zoom: 8,
-  scrollwheel: true,
+  scrollwheel: false,
   mapTypeId: google.maps.MapTypeId.ROADMAP
 }
 var map = new google.maps.Map(mapCanvas, mapOptions)
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
