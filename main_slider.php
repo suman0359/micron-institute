7c3c14055bc05d882c60864f4b02982fca41701b
Tasfir Hossain Suman <tasfir_suman@yahoo.com>
2015-11-29 18:11:24 +0600

    add php slider and customise custom css for slider margin-top

--- main_slider.php
+++ main_slider.php
@@ -11,7 +11,7 @@
                     <!-- Wrapper for slides -->
                     <div class="carousel-inner" role="listbox">
                         <div class="item active">
-                            <img src="images/slider/micron_php.jpg" alt="Slider Image One">
+                            <img src="images/slider/city-landscape-slider.jpg" alt="Slider Image One">
                             <div class="carousel-caption">
                                 This a very Simple Image Caption
                             </div>
